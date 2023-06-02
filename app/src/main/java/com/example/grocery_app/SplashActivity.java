7941d229d5cf3b30eaf015e5cff99afbdc01148c
HiAnhhanhh <hoanganh124356@gmail.com>
2023-06-03 00:09:09 +0700

    Layout Login, Register

---
+++ app/src/main/java/com/example/grocery_app/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.grocery_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.grocery_app.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashBinding.inflate(LayoutInflater.from(this));
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(binding.getRoot());
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent (SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+    }
+}
